e065fd2555e75980b6ef63acde5df4650d4e410b
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-09-12 11:09:00 +0200

    Add GlobalStyles.js

---
+++ client/src/GlobalStyles.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Global, css } from "@emotion/core";
+
+const GlobalStyles = () => {
+  return (
+    <Global
+      styles={css`
+        *,
+        *:before,
+        *:after {
+          box-sizing: border-box;
+          margin: 0;
+        }
+      `}
+    />
+  );
+};
+
+export default GlobalStyles;
