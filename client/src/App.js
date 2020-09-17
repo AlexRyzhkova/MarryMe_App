eaca571b8b3bf329ac9d670e65e466be245fb63c
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-09-17 07:45:51 +0200

    Add ToDoListItem to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ToDoListItem from "./components/ToDoListItem/ToDoListItem";
 import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <div>
       <GlobalStyles />
-      <h1>Hello</h1>
+      <ToDoListItem />
     </div>
   );
 }
