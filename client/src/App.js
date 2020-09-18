5c8232af5bd09da4ad2aeb14bedc8e0a05d00fa7
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-09-18 08:26:20 +0200

    Add header to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./components/Header/Header";
 import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <div>
       <GlobalStyles />
+      <Header />
       <h1>Hello</h1>
     </div>
   );
