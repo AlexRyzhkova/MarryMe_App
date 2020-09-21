201f8852332e9b489269d7f68d4d07822ac5dcf6
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-09-21 16:05:34 +0200

    Remove padding from storybook.

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -2,6 +2,7 @@ import React from "react";
 import GlobalStyles from "../src/GlobalStyles";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: "fullscreen",
 };
 const withGlobalStyles = (Story, context) => {
   return (
