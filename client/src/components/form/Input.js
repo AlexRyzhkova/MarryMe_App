0474ea2df7f9284b4789641d7e51d3ede78ab14d
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-09-25 10:09:35 +0200

    Add Button component and story

--- client/src/components/form/Input.js
+++ client/src/components/form/Input.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-// import PropTypes from "prop-types";
 
 const Input = ({ type, value, onChange, placeholder }) => {
   const [title, setTitle] = useState("");
