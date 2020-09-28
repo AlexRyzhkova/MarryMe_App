df9bcb28b182d681933e54692ed183aff9e2530b
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-09-28 14:36:41 +0200

    Add SignUpButton component

---
+++ client/src/components/signUpButton/SignUpButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const SignUpButton = () => {
+  return <StyledSignUpButton>Registrieren</StyledSignUpButton>;
+};
+
+export default SignUpButton;
+
+const StyledSignUpButton = styled.button`
+  text-align: center;
+  border-radius: 1.8em;
+  padding: 0.6em 1.8em;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  color: var(--font-color-light);
+  background-color: #ad9494;
+`;
