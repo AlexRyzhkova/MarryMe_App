9f5cc924022e370aa8c93c3b35069ea986a00e3a
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-09-28 21:53:11 +0200

    Add styling to the SignUpForm

--- client/src/components/signUpForm/SignUpForm.js
+++ client/src/components/signUpForm/SignUpForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import styled from "@emotion/styled";
+import styled from "@emotion/styled";
 import { useForm } from "react-hook-form";
 import SignUpButton from "../signUpButton/SignUpButton";
 
@@ -8,7 +8,7 @@ const SignUpForm = () => {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div>
+    <Form>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Dein Name</label>
         <input
@@ -33,7 +33,36 @@ const SignUpForm = () => {
         {errors.DateTime && <p>Das ist ein Pflichtfeld</p>}
         <SignUpButton />
       </form>
-    </div>
+    </Form>
   );
 };
 export default SignUpForm;
+
+//styling
+
+const Form = styled.div`
+  form {
+    display: grid;
+    grid-template-columns: auto 60%;
+    grid-template-rows: 1fr 1fr 1fr auto;
+    align-content: space-between;
+    align-items: space-between;
+    grid-row-gap: 1.8em;
+    grid-column-gap: 3em;
+  }
+  button {
+    margin-top: 3.1em;
+    grid-column: 1/3;
+    justify-self: center;
+  }
+  label {
+    align-self: center;
+  }
+  input {
+    width: 80%;
+    outline: none;
+    border: none;
+    border-bottom: 1px solid #383636;
+    background-color: transparent;
+  }
+`;
